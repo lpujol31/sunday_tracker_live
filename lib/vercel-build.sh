a9f45978b057e2052539090bca6d4f124b7a69f5
Laurent Pujol <laurent.pujol@novadys.fr>
2026-05-19 22:01:28 +0200

    Fix Flutter Web routing for Vercel

---
+++ lib/vercel-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git -b stable --depth 1
+export PATH="$PATH:$PWD/flutter/bin"
+
+flutter config --enable-web
+flutter pub get
+flutter build web --release
